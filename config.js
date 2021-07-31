1f156a2d9ec9b8257e379b737f74d4d20499daff
TRIDSN1596 <88089034+TRIDSN1596@users.noreply.github.com>
2021-07-31 23:21:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Minh Anh à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introTitle: 'Thằng loz thầy !',
+    introDesc: `djt me may
+   
+   bố đấm mày nhé
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Nhấn Tiếp Tục ĐI nè',
     title: '',
-    desc: 'Em có thích anh không ',
+    desc: 'thầy đánh như ncc',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh ',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Bữa nào đi satea với anh nhaaa.',
-    btnAccept: 'Okiiiii lun <3',
+    question:' ',
+    reply: '',
+    messDesc: '.',
+    btnAccept: ',
     messLink: 'http://m.me/tridsn1596' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
